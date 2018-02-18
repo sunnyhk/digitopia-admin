5bd96db59aad24e82217b110749ceb31f028be37
Sunny Pan <sunnyhk@yahoo.com>
2018-02-18 23:20:35 +0800

    Try to use ajax call for the buttons

---
+++ client/dist/js/custom.js
@@ -0,0 +1,12 @@
+var ajax_post = function(url, data) {
+	var posting = $.post(url, data);
+
+	posting.done(function() {
+		alert("Success");
+	})
+	.fail(function() {
+		alert("Failed");
+	})
+	.always(function() {
+	});
+};
